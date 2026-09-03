c100f706b9762ca3c3df4db1e3cead517463a183
Diwyti <94397548+Diwyti@users.noreply.github.com>
2026-09-03 20:10:34 +0500

    Add files via upload

---
+++ lab1/5.py
@@ -0,0 +1,8 @@
+d_grani = int(input("Введите длину граней квадрата: "))
+
+p_grani = d_grani ** 2
+print("Площадь квадрата:", p_grani)
+perimetr = 4 * d_grani
+print("Периметр квадрата:", perimetr)
+diagonal = (2 * (d_grani ** 2)) ** 0.5
+print("Длина диагонали квадрата:", diagonal)
