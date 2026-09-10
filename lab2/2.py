a2bda0e18fd5dc72ce2d692e40b73a20e578b0b7
Diwyti <94397548+Diwyti@users.noreply.github.com>
2026-09-10 15:48:53 +0500

    Update and rename 1 to 2.py

---
+++ lab2/2.py
@@ -0,0 +1,10 @@
+
+a = input("введите значение a: ")
+b = input("введите значение b: ")
+c = input("введите значение c: ")
+
+sum = int(a) + int(b) + int(c)
+print("Сумма значений:", sum)
+average = sum / 3
+print("Среднее значение:", average)
+
